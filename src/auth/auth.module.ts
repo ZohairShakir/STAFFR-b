ae7e421ee1fc7f23f23f8f93ea89a23afb508d47
Zoher Shakir <zohairshakir2007@gmail.com>
2026-06-07 20:48:08 +0530

    changes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,11 +4,15 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
+import { GatewayModule } from '../gateway/gateway.module';
+import { SlackModule } from '../slack/slack.module';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({}),
+    GatewayModule,
+    SlackModule,
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
